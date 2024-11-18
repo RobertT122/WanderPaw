76a3f387acf32ab4b88073d539d571f4e8b438d2
grtetrault <92237319+grtetrault@users.noreply.github.com>
2024-11-18 18:50:46 -0500

    Basic herding implemenetation (#4)

--- webpack.config.js
+++ webpack.config.js
@@ -2,25 +2,25 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
 
 module.exports = {
-  mode: "development",
-  entry: "./src/game.js",
-  output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "dist"),
-  },
-  module: {
-    rules: [
-      {
-        test: /\.png/,
-        type: "asset/resource",
-      },
-    ],
-  },
-  plugins: [new HtmlWebpackPlugin()],
-  devServer: {
-    static: {
-      directory: path.join(__dirname, "dist"),
+    mode: "development",
+    entry: "./src/game.js",
+    output: {
+        filename: "main.js",
+        path: path.resolve(__dirname, "dist"),
     },
-    port: 9000,
-  },
+    module: {
+        rules: [
+            {
+                test: /\.png/,
+                type: "asset/resource",
+            },
+        ],
+    },
+    plugins: [new HtmlWebpackPlugin()],
+    devServer: {
+        static: {
+            directory: path.join(__dirname, "dist"),
+        },
+        port: 9000,
+    },
 };
